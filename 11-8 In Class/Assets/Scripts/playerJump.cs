dc5cc86d5d728558bbefc3585919c7e5a21537b1
angelatangela <mangela@Angelas-MBP.fios-router.home>
2016-11-15 13:22:41 -0500

    Week 10 HW

---
+++ 11-8 In Class/Assets/Scripts/playerJump.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerJump : MonoBehaviour {
+
+	public float jumpStrength;
+	public float gravity;
+	public Vector3 yVelocity;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			//set ther yvelocity to equal (0,jumpstrength0) when we press space 
+			yVelocity = new Vector3 (0, jumpStrength, 0);
+		}
+				
+		//give the player upwards velocity;
+		transform.position += new Vector3 (0, jumpStrength, 0);
+
+		//give the player upwards velocity;
+		transform.position += yVelocity * Time.deltaTime;
+
+		if (transform.position.y > 1) {
+			
+			yVelocity.y -= gravity;
+		} else {
+			yVelocity.y = 0;
+
+			transform.position = new Vector3 (0, 1.0f, 0);
+
+		}
+
+		Debug.Log (yVelocity);
+
+	
+	}
+}
---
+++ 11-8 In Class/Assets/Scripts/wasdmoving.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wasdmoving : MonoBehaviour {
+
+	private Transform myTransform;
+	public float moveSpeed;
+
+
+	// Use this for initialization
+	void Start () {
+		myTransform = GetComponent<Transform> ();
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		//Delta TIme - the amount of time that has gone by in each frama
+
+		if (Input.GetKey (KeyCode.W)) {
+			myTransform.position += transform.forward * Time.deltaTime * moveSpeed;
+		}
+
+		if (Input.GetKey(KeyCode.S)){
+			myTransform.position -= transform.forward;
+		}
+	}
+}
